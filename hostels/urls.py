6fdae5667783b9bbe012ff5ec13ac83f80a1928c
shaikhmaaz207 <animeustad@gmail.com>
2026-06-04 17:03:27 +0530

    Fix hostel urls.py to include HA-12 images endpoint

--- hostels/urls.py
+++ hostels/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import HostelListView, CreateHostelView, UploadHostelImageView
+from .views import HostelListView, CreateHostelView, UploadHostelImageView, HostelImageListCreateView
 
 urlpatterns = [
     path('', HostelListView.as_view(), name='hostel-list'),
     path('create/', CreateHostelView.as_view(), name='create-hostel'),
     path('<int:hostel_id>/upload-image/', UploadHostelImageView.as_view(), name='upload-image'),
+    path('<int:hostel_id>/images/', HostelImageListCreateView.as_view(), name='hostel-images'),
 ]
-from .views import HostelImageListCreateView
-
-path('hostels/<int:hostel_id>/images/', HostelImageListCreateView.as_view(), name='hostel-images'),
